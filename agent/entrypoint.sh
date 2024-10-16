c1e9a1f9cbbab46b35c2c21fb4193084a0673b2d
Ekam Puri Nieto <ekam.purin@um.es>
2024-10-16 11:12:34 +0200

    Automatically start the OpenC2 Actuator
    Signed-off-by: Ekam Puri Nieto <ekam.purin@um.es>

--- agent/entrypoint.sh
+++ agent/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 /etc/init.d/wazuh-agent start
-tail -F /var/ossec/logs/ossec.log
+cd /agent/openc2_actuator
+mkfifo actuator_logs
+python3 -m actuator | tee actuator_logs | tail -F actuator_logs /var/ossec/logs/ossec.log 2>&1
